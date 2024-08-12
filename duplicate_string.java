dfaa9a773d5dc4eb8b3a1e12ff7088d42da403ba
hack-boopathi <boopathig280@gmail.com>
2024-08-12 08:41:27 -0700

    string

---
+++ duplicate_string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class duplicate_string {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a string to find duplicate data:");
+		String s=sc.nextLine();	
+		String sa=s.toLowerCase();
+		char [] ch = sa.toCharArray();
+		int count=0;
+		for (int i=0;i<ch.length;i++) {
+			for (int j=i+1;j<ch.length;j++) {
+				if(ch[i]==ch[j]) {
+					System.out.println("duplicate string :"+ch[j]);
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
